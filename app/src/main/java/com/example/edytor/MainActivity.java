fb5f7d6ac4b5a85ddbbef3e71035ce4c2e949562
ktos <nowakstas6@gmail.com>
2021-10-25 15:18:46 +0200

    dodano nazwy i id

--- app/src/main/java/com/example/edytor/MainActivity.java
+++ app/src/main/java/com/example/edytor/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.edytor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox pokaz,ukryj;
+    EditText rotacja, skala;
+    ImageView obraz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    pokaz.findViewById(R.id.pokaz_obraz);
+    ukryj.findViewById(R.id.zchowaj_obraz);
+    rotacja.findViewById(R.id.rotacja);
+    skala.findViewById(R.id.skala);
+    obraz.findViewById(R.id.obraz);
+
     }
 }
