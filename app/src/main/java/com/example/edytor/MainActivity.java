af5c6729f11009c25a1ad6eff678d5eb5a7bab71
ktos <nowakstas6@gmail.com>
2021-10-25 15:53:08 +0200

    pokaz/schowaj

--- app/src/main/java/com/example/edytor/MainActivity.java
+++ app/src/main/java/com/example/edytor/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.edytor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -17,11 +19,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    pokaz.findViewById(R.id.pokaz_obraz);
-    ukryj.findViewById(R.id.zchowaj_obraz);
-    rotacja.findViewById(R.id.rotacja);
-    skala.findViewById(R.id.skala);
-    obraz.findViewById(R.id.obraz);
+    pokaz=(CheckBox) findViewById(R.id.pokaz_obraz);
+    ukryj=(CheckBox) findViewById(R.id.zchowaj_obraz);
+    rotacja=(EditText) findViewById(R.id.rotacja);
+    skala=(EditText) findViewById(R.id.skala);
+    obraz=(ImageView) findViewById(R.id.obraz);
+
+    pokaz.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            if(isChecked==true)
+            {
+                obraz.setVisibility(View.VISIBLE);
+            }
+        }
+    });
+    ukryj.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            if(isChecked==true){
+                obraz.setVisibility(View.INVISIBLE);
+            }
+        }
+    });
 
     }
 }
